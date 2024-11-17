e525b496dd601bf0cf7d73914305835300441151
yashas8197 <yashasjr7@gmail.com>
2024-11-17 15:07:19 +0530

    Add PORT to env

--- insightscope-be/server.js
+++ insightscope-be/server.js
@@ -1,6 +1,6 @@
 const { app } = require("./index.js");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server connected to port http://localhost:${PORT}`);
 });
